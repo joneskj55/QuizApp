e36ee711d6360a41de18a3a1ab1c1bacdce4f65b
Kevin Jones <joneskj55@gmail.com>
2017-04-10 17:55:43 -0500

    Multiple choice questions functional (first stages)

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,12 +2,62 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
+    boolean answerIsCorrect = true; //might not need this variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void questionOne(View view) {
+        RadioButton numberOneCorrectAnswer = (RadioButton) findViewById(R.id.choice_2_question_1);
+        boolean answerForNumberOne = numberOneCorrectAnswer.isChecked();
+        if (answerForNumberOne) {
+            score = score + 1;
+        }
+    }
+
+    public void questionTwo() {
+        String answerForNumberTwo = "This is it.";
+        EditText numberTwoUserAnswer = (EditText) findViewById(R.id.answer_question_2);
+        String userAnswer = numberTwoUserAnswer.getText().toString();
+
+        if (userAnswer == answerForNumberTwo) {
+            score = score + 1;
+        }
+    }
+
+    public void questionThree() {
+
+    }
+
+    public void questionFour(View view) {
+        RadioButton numberFourCorrectAnswer = (RadioButton) findViewById(R.id.choice_1_question_4);
+        boolean answerForNumberFour = numberFourCorrectAnswer.isChecked();
+        if (answerForNumberFour) {
+            score = score + 1;
+        }
+    }
+
+    public void questionFive(View view) {
+        RadioButton numberFiveCorrectAnswer = (RadioButton) findViewById(R.id.choice_3_question_5);
+        boolean answerForNumberFive = numberFiveCorrectAnswer.isChecked();
+        if (answerForNumberFive) {
+            score = score + 1;
+        }
+    }
+
+    public void submitTest(View view) {
+//        questionTwo();
+        Toast.makeText(this, "Out of a total score of 5, your scored " + score, Toast.LENGTH_SHORT).show();
     }
 }
